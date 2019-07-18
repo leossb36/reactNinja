f42347a4f3986ad991e2e586bbfe642077bc8784
Leonardo Barreiros <leossb36@gmail.com>
2019-07-18 15:35:18 -0300

    React + webpack

---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,13 @@
+// para bundle final muda o publicPath para um local fisico, no caso dist -> onde está o arquivo bundle.js
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/' // gera um arquivo em memória
+  }
+}
