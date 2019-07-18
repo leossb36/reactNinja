d9252bcc12f0bd691b40dd9b266d21465d3c76ae
Leonardo Barreiros <leossb36@gmail.com>
2019-07-18 17:14:43 -0300

    Criando uma aplicação com webpack

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,7 +1,16 @@
 'use strict'
+var React = require('react')
+// function sum(var1, var2) {
+//   return var1 + var2
+// }
 
-function sum(var1, var2) {
-  return var1 + var2
-}
+// module.exports = sum
 
-module.exports = sum
+
+var Title = React.createClass({
+  render: function() {
+    return React.createElement('h1', null, 'Titulo')
+  }
+})
+
+module.exports = Title
--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,7 +1,15 @@
 // console.log('webpack está funcionando!!')
 
 'use strict'
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
+// var sum = require('./app')
 
-var sum = require('./app')
+// console.log(sum(1,2))
 
-console.log(sum(1,2))
+
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+)
